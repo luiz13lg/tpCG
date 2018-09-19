8f4e66a49d59ee84370b71a18ea896666a092fce
Luiz G <luiz13lg13@gmail.com>
2018-09-19 20:37:09 -0300

    Adição da paleta de cores
    Foi adicionada a paleta de cores e sua funcionalidade.

--- src/iu/iuPrincipal.java
+++ src/iu/iuPrincipal.java
@@ -16,13 +16,13 @@ public class iuPrincipal extends javax.swing.JFrame {
     private int controle = 0;   //variavel de controle para funçoes do programa
     private int x1, x2, y1, y2; //variaveis de posicao do mouse
     private int aux = 0;    //var para acesso de funçoes em duas etapas
-    private Color cor;      //cor provinda da paleta de cores
+    private Color cor = Color.BLACK;      //cor provinda da paleta de cores
+    private iuPaleta paleta = new iuPaleta();
     /**
      * Creates new form iuPrincipal
      */
     public iuPrincipal() {
         initComponents();
-        paleta.setColor(Color.BLACK);   //setando a paleta para preto ao abrir a janela
     }
 
     /**
@@ -39,7 +39,9 @@ public class iuPrincipal extends javax.swing.JFrame {
         jrLinha = new javax.swing.JRadioButton();
         jrCirculo = new javax.swing.JRadioButton();
         jrLivre = new javax.swing.JRadioButton();
-        paleta = new javax.swing.JColorChooser();
+        jMenuBar1 = new javax.swing.JMenuBar();
+        jMenu1 = new javax.swing.JMenu();
+        jMenuItem1 = new javax.swing.JMenuItem();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setAutoRequestFocus(false);
@@ -62,7 +64,7 @@ public class iuPrincipal extends javax.swing.JFrame {
         jpDesenho.setLayout(jpDesenhoLayout);
         jpDesenhoLayout.setHorizontalGroup(
             jpDesenhoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 0, Short.MAX_VALUE)
+            .addGap(0, 537, Short.MAX_VALUE)
         );
         jpDesenhoLayout.setVerticalGroup(
             jpDesenhoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -79,24 +81,33 @@ public class iuPrincipal extends javax.swing.JFrame {
         jrLivre.setSelected(true);
         jrLivre.setText("Desenho Livre");
 
+        jMenu1.setText("Funções");
+
+        jMenuItem1.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_C, java.awt.event.InputEvent.SHIFT_MASK | java.awt.event.InputEvent.CTRL_MASK));
+        jMenuItem1.setText("Mudar Cor");
+        jMenuItem1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItem1ActionPerformed(evt);
+            }
+        });
+        jMenu1.add(jMenuItem1);
+
+        jMenuBar1.add(jMenu1);
+
+        setJMenuBar(jMenuBar1);
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
+                .addComponent(jpDesenho, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addComponent(jpDesenho, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jrLinha)
-                            .addComponent(jrCirculo)
-                            .addComponent(jrLivre))
-                        .addGap(0, 0, 0))
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                        .addGap(0, 13, Short.MAX_VALUE)
-                        .addComponent(paleta, javax.swing.GroupLayout.PREFERRED_SIZE, 615, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                    .addComponent(jrLinha)
+                    .addComponent(jrCirculo)
+                    .addComponent(jrLivre))
                 .addContainerGap())
         );
         layout.setVerticalGroup(
@@ -111,15 +122,14 @@ public class iuPrincipal extends javax.swing.JFrame {
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                         .addComponent(jrLivre))
                     .addComponent(jpDesenho, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(paleta, javax.swing.GroupLayout.PREFERRED_SIZE, 328, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap())
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
     private void jpDesenhoMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jpDesenhoMouseClicked
+        cor = paleta.getCor();
         if (jrLinha.isSelected())
             pintarReta(jpDesenho.getGraphics(), evt.getX(), evt.getY());
         else if (jrCirculo.isSelected())
@@ -127,9 +137,14 @@ public class iuPrincipal extends javax.swing.JFrame {
     }//GEN-LAST:event_jpDesenhoMouseClicked
 
     private void jpDesenhoMouseDragged(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jpDesenhoMouseDragged
+        cor = paleta.getCor();
         if(jrLivre.isSelected())
                 pintar(jpDesenho.getGraphics(), evt.getX(), evt.getY());
     }//GEN-LAST:event_jpDesenhoMouseDragged
+
+    private void jMenuItem1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem1ActionPerformed
+        paleta.setVisible(true);
+    }//GEN-LAST:event_jMenuItem1ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -175,30 +190,29 @@ public class iuPrincipal extends javax.swing.JFrame {
             x2 = x;
             y2 = y;
             aux = 0;
-            cor = paleta.getColor();
             g.setColor(cor);
             g.drawLine(x1, y1, x2, y2);
         }
     }
     
     void pintarCirculo(Graphics g, int x, int y){
-        cor = paleta.getColor();
         g.setColor(cor);
         g.drawOval(x, y, 6, 6);
     }
     
     void pintar(Graphics g, int x, int y){  //pintando um pixel
-        cor = paleta.getColor();
         g.setColor(cor);
         g.drawLine(x, y, x, y);
     }
     
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.ButtonGroup buttonGroup1;
+    private javax.swing.JMenu jMenu1;
+    private javax.swing.JMenuBar jMenuBar1;
+    private javax.swing.JMenuItem jMenuItem1;
     private javax.swing.JPanel jpDesenho;
     private javax.swing.JRadioButton jrCirculo;
     private javax.swing.JRadioButton jrLinha;
     private javax.swing.JRadioButton jrLivre;
-    private javax.swing.JColorChooser paleta;
     // End of variables declaration//GEN-END:variables
 }
